594bbfebf151501e7332134a400f1cbd85df0eae
Jo-Young-Woo <c21114@kma.ac.kr>
2025-11-02 14:48:22 +0900

    alarmcomplete- 이전 커밋은 전에 파일과 겹쳐서 실행안됌

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -82,6 +82,7 @@ typedef int tid_t;
    blocked state is on a semaphore wait list. */
 struct thread
   {
+   int64_t wakeup;
     /* Owned by thread.c. */
     tid_t tid;                          /* Thread identifier. */
     enum thread_status status;          /* Thread state. */
@@ -137,5 +138,8 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
+
+void thread_sleep(int64_t ticks);
+void thread_awake(int64_t ticks);
 
 #endif /* threads/thread.h */
